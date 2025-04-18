324802c238a8c64e71ee67e3d8dd37067db19641
mmoor151 <mmoor@unc.edu>
2025-04-17 21:53:52 -0400

    done

--- exercises/ex04/bear.py
+++ exercises/ex04/bear.py
@@ -1,22 +1,24 @@
-__author__ = "730765813"
-
 """File to define Bear class."""
+
+__author__ = "730765813"
 
 
 class Bear:
+    """Define bear class."""
+
     age: int
     hunger_score: int
 
     def __init__(self) -> None:
-        """Allow for the creation of multiple bear nodes"""
+        """Allow for the creation of multiple bear nodes."""
         self.age: int = 0
         self.hunger_score: int = 0
 
     def one_day(self) -> None:
-        """How one day impacts a bear"""
+        """How one day impacts a bear."""
         self.age += 1
         self.hunger_score -= 1
 
     def eat(self, num_fish: int) -> None:
-        """The bear eats a fish"""
+        """The bear eats a fish."""
         self.hunger_score += num_fish
--- exercises/ex04/fish.py
+++ exercises/ex04/fish.py
@@ -1,15 +1,17 @@
-__author__ = "730765813"
-
 """File to define Fish class."""
+
+__author__ = "730765813"
 
 
 class Fish:
+    """Define fish class."""
+
     age: int
 
     def __init__(self) -> None:
-        """Allow for the creation of multiple fish nodes"""
+        """Allow for the creation of multiple fish nodes."""
         self.age: int = 0
 
     def one_day(self) -> None:
-        """How one day impacts a fish"""
+        """How one day impacts a fish."""
         self.age += 1
